0c060b16ca91b504a3a40b775ff0e90656ee063e
prasanna61g6 <155382664+prasanna61g6@users.noreply.github.com>
2025-02-15 13:28:28 +0530

    Update SelectionSort.c

--- SelectionSort.c
+++ SelectionSort.c
@@ -1,9 +1,9 @@
 //Algorithm for Insertion Sort
 //----------------------------
-// Step 1: Run a loop from 0 to N - 2 using i variable
+// Step 1: Run a loop from 0 to N  using i variable
 // Step 2: Create an integer variable called
 //	     	min_index and store i value in it
-//	    	Run a loop from i + 1 to N - 1 using a variable j
+//	    	Run a loop from i + 1 to N  using a variable j
 // Step 3:	Compare if
 //				A[j] < A[min_index]
 //				then, set min_index = j
