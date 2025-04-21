6495ba119b8a1f94d6a99e071022a626a8c9cd6e
prasanna61g6 <155382664+prasanna61g6@users.noreply.github.com>
2025-04-21 10:12:22 +0530

    Add files via upload

---
+++ QuickSort.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int Merge(int A[], int low, int high, int pivot) {
+	int key = A[pivot];
+	int s = low;
+	int e = high;
+	while(s < e) {
+		while(A[s] <= key) {
+			s++;
+		}
+		while(A[e] > key) {
+			e--;
+		}
+		if(s < e) {
+			int temp = A[s];
+			A[s] = A[e];
+			A[e] = temp;
+		}
+	}
+	int temp = A[e];
+	A[e] = A[pivot];
+	A[pivot] = temp;
+	return e;
+}
+void QuickSort(int A[], int low, int high) {
+	if(low < high) {
+		int pivot = low;
+		pivot = Merge(A, low, high, pivot);
+		QuickSort(A, low, pivot - 1);
+		QuickSort(A, pivot + 1, high);
+	}
+}
+int main() {
+	int n;
+	scanf("%d",&n);
+	int arr[n];
+	for(int i=0;i<n;i++) {
+		scanf("%d",&arr[i]);
+	}
+	QuickSort(arr,0,n-1);
+	for(int i=0;i<n;i++) {
+		printf("%d ",arr[i]);
+	}
+}
